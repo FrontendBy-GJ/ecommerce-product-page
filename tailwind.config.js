68c876bbd453318bf0e37cd5e856b8e6cf83eb88
Genaro Garcia <genaro.dev@outlook.com>
2023-05-05 10:16:56 -0500

    install prettier-plugin-tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        Primary: {
+          orange: 'hsl(26, 100%, 55%)',
+          paleOrange: 'hsl(25, 100%, 94%)',
+        },
+        Neutral: {
+          VeryDarkBlue: 'hsl(220, 13%, 13%)',
+          DarkGrayishBlue: 'hsl(219, 9%, 45%)',
+          GrayishBlue: 'hsl(220, 14%, 75%)',
+          LightGrayishBlue: 'hsl(223, 64%, 98%)',
+        },
+      },
+    },
+    fontFamily: {
+      kumbhSans: ['Kumbh Sans', 'sans-serif'],
+    },
+  },
+  plugins: [],
+};
