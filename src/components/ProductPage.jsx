347d0889ad2f872a157e8c30b97776ccf2158186
Genaro Garcia <genaro.dev@outlook.com>
2023-05-12 10:48:42 -0500

    Add Product page

---
+++ src/components/ProductPage.jsx
@@ -0,0 +1,188 @@
+import CartIcon from '../assets/icons/CartIcon';
+import PreviousIcon from '../assets/icons/PreviousIcon';
+import NextIcon from '../assets/icons/NextIcon';
+import MinusIcon from '../assets/icons/MinusIcon';
+import PlusIcon from '../assets/icons/PlusIcon';
+import useCartContext from '../hooks/useCartContext';
+import { useState } from 'react';
+import { products } from '../data';
+import Lightbox from './Lightbox';
+import ImageThumbnail from './ImageThumbnail';
+import GalleryButton from './GalleryButton';
+
+const ProductPage = () => {
+  const [currentImage, setCurrentImage] = useState(0);
+  const [slideIndex, setSlideIndex] = useState(0);
+  const [clickedImage, setClickedImage] = useState(null);
+
+  const { cart, setCart, formatPrice, setEmptyCart } = useCartContext();
+
+  const mainImages = products[0].images.main;
+
+  const handlePrevImg = () => {
+    if (slideIndex !== 0) {
+      setSlideIndex(slideIndex - 1);
+    } else if (slideIndex === 0) {
+      setSlideIndex(mainImages.length - 1);
+    }
+  };
+
+  const handleNextImg = () => {
+    if (slideIndex !== mainImages.length - 1) {
+      setSlideIndex(slideIndex + 1);
+    } else if (slideIndex === mainImages.length - 1) {
+      setSlideIndex(0);
+    }
+  };
+
+  const handleMinusBtn = () => {
+    if (cart.qty <= 0) {
+      return;
+    } else {
+      setCart({ ...cart, qty: cart.qty - 1 });
+    }
+  };
+
+  const handlePlusBtn = () => {
+    setCart({ ...cart, qty: cart.qty + 1 });
+  };
+
+  const handleAddToCart = (productName, productPrice, productImage) => {
+    setCart({
+      ...cart,
+      product: productName,
+      qty: cart.qty,
+      price: productPrice,
+      total: cart.qty * productPrice,
+      image: productImage,
+    });
+
+    setEmptyCart(false);
+  };
+
+  const handleImgClick = () => {
+    setClickedImage(mainImages[currentImage]);
+
+    if (typeof window != 'undefined' && window.document) {
+      document.body.style.overflow = 'hidden';
+    }
+  };
+
+  const closeLightbox = () => {
+    setClickedImage(null);
+
+    document.body.style.overflow = 'unset';
+  };
+
+  return (
+    <>
+      <div className="bg-white md:mx-auto md:flex md:max-w-7xl md:gap-10 md:px-4 md:py-16 lg:px-10">
+        <div className="relative md:flex-1 md:self-center lg:max-w-lg">
+          <div className="absolute inset-0 flex items-center justify-between px-4 md:hidden">
+            <GalleryButton onClick={handlePrevImg} Icon={PreviousIcon} />
+            <GalleryButton onClick={handleNextImg} Icon={NextIcon} />
+          </div>
+          <div className="hidden md:block md:cursor-pointer">
+            <img
+              onClick={handleImgClick}
+              src={mainImages[currentImage]}
+              alt=""
+              className="h-full w-full rounded-xl object-cover"
+            />
+            <div className="mt-6 flex justify-center gap-8">
+              {products[0].images.thumbnails.map((t, index) => (
+                <ImageThumbnail
+                  key={index}
+                  src={t}
+                  alt={''}
+                  onClick={() => setCurrentImage(index)}
+                  isSelected={index === currentImage}
+                />
+              ))}
+            </div>
+          </div>
+          <div className="h-[300px] md:hidden">
+            {products[0].images.main.map((img, index) => (
+              <img
+                key={index}
+                src={img}
+                alt=""
+                className={`${index !== slideIndex ? 'hidden' : ''} ${
+                  index !== 0 ? 'object-top' : ''
+                } h-full w-full object-cover`}
+              />
+            ))}
+          </div>
+        </div>
+
+        <div className="px-5 font-kumbhSans md:flex-1 md:self-center lg:max-w-xl xl:px-12">
+          <h3 className="mt-5 text-xs font-bold tracking-wider text-Primary-orange lg:text-base">
+            SNEAKER COMPANY
+          </h3>
+          {products.map((p) => (
+            <div key={p.id}>
+              <h2 className="my-3 text-3xl font-bold text-Neutral-VeryDarkBlue lg:max-w-md xl:text-5xl">
+                {p.product}
+              </h2>
+              <p className="tracking-tight text-Neutral-DarkGrayishBlue md:my-5 lg:text-lg">
+                {p.info}
+              </p>
+              <div className="mt-5 flex items-center justify-between md:flex-col md:items-start">
+                <div className="flex items-center gap-4">
+                  <span className="text-2xl font-bold tracking-wide lg:text-3xl">
+                    {formatPrice(p.price)}
+                  </span>
+                  <span className="rounded bg-Primary-paleOrange px-1.5 font-bold text-Primary-orange">
+                    50%
+                  </span>
+                </div>
+                <s className="text-sm font-bold text-Neutral-GrayishBlue md:mt-2 lg:text-base">
+                  {formatPrice(p.originalPrice)}
+                </s>
+              </div>
+
+              <div className="md:mt-5 md:items-start lg:gap-5 xl:mt-16 xl:flex">
+                <div className="mt-5 flex justify-between rounded-lg bg-Neutral-LightGrayishBlue px-5 py-4 md:mt-0 xl:w-64">
+                  <button
+                    onClick={handleMinusBtn}
+                    className="text-Primary-orange hover:text-Primary-orange/70"
+                  >
+                    <MinusIcon />
+                  </button>
+                  <span className="font-bold">{cart.qty}</span>
+                  <button
+                    onClick={handlePlusBtn}
+                    className="text-Primary-orange hover:text-Primary-orange/70"
+                  >
+                    <PlusIcon />
+                  </button>
+                </div>
+                <button
+                  onClick={() =>
+                    handleAddToCart(p.product, p.price, p.images.main[0])
+                  }
+                  disabled={cart.qty === 0}
+                  className="mb-16 mt-3 flex w-full justify-center gap-4 rounded-lg bg-Primary-orange py-4 text-Neutral-LightGrayishBlue shadow-2xl shadow-Primary-orange/50 transition-colors hover:bg-Primary-orange/70 md:mt-5 xl:mt-0"
+                >
+                  <CartIcon />
+                  <span className="text-sm font-bold">Add to cart</span>
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {clickedImage && (
+        <Lightbox
+          closeLightbox={closeLightbox}
+          mainImages={mainImages}
+          clickedImage={clickedImage}
+          setClickedImage={setClickedImage}
+        />
+      )}
+    </>
+  );
+};
+
+export default ProductPage;
