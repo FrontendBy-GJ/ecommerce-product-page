7dfe8caea178c3ce8be7fc98b004187f024fb46b
Genaro Garcia <genaro.dev@outlook.com>
2023-05-12 10:58:50 -0500

    Wrapped Nav and ProductPage in CartContextProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import { useState } from 'react';
+import Nav from './components/Nav';
+import ProductPage from './components/ProductPage';
+import CartContextProvider from './context/CartContext';
+
 function App() {
-  return <></>;
+  const [openMenu, setOpenMenu] = useState(false);
+
+  const handleMenuClick = () => setOpenMenu((prev) => !prev);
+
+  return (
+    <>
+      <CartContextProvider>
+        <Nav openMenu={openMenu} handleMenuClick={handleMenuClick} />
+        <ProductPage />
+      </CartContextProvider>
+    </>
+  );
 }
 
 export default App;
