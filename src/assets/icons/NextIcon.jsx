d2880665229115390632cf0ea702109707f13af3
Genaro Garcia <genaro.dev@outlook.com>
2023-05-12 10:40:34 -0500

    Add new icon components

---
+++ src/assets/icons/NextIcon.jsx
@@ -0,0 +1,14 @@
+const NextIcon = (props) => {
+  return (
+    <svg {...props} width="13" height="18" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="m2 1 8 8-8 8"
+        strokeWidth="3"
+        fill="currentColor"
+        fillRule="evenodd"
+      />
+    </svg>
+  );
+};
+
+export default NextIcon;
---
+++ src/assets/icons/PreviousIcon.jsx
@@ -0,0 +1,14 @@
+const PreviousIcon = (props) => {
+  return (
+    <svg {...props} width="12" height="18" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M11 1 3 9l8 8"
+        strokeWidth="3"
+        fill="currentColor"
+        fillRule="evenodd"
+      />
+    </svg>
+  );
+};
+
+export default PreviousIcon;
